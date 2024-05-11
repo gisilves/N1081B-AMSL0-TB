01e7eccfa601e3a9973802cd16f14da304f9da49
Mattia Barbanera <mattia.barbanera@gmail.com>
2024-05-11 07:44:05 +0000

    up

--- disable_all_triggers.py
+++ disable_all_triggers.py
@@ -1,7 +1,7 @@
 import sys
 from datetime import datetime
 from N1081B_sdk import N1081B
-from client_charge_tagger_L0BT_20231022 import *
+#from client_charge_tagger_L0BT_20231022 import *
 
 def disable_master_trigger():
     N1081B_device1.configure_digital_generator(N1081B.Section.SEC_C,False,False,False,False)
@@ -31,26 +31,27 @@ def read_scalers():
     scaler_3 = next(item['value'] for item in lemo_counters if item['lemo'] == target_lemo)
     
     now = datetime.now()
-    print(now.strftime("%d/%m/%Y %H:%M:%S") + "\tTotal beam triggers: " + str(scaler_0))
-    print(now.strftime("%d/%m/%Y %H:%M:%S") + "\tTotal cal triggers: " + str(scaler_1))
+    print(now.strftime("%d/%m/%Y %H:%M:%S") + "\tTotal beam triggers: " + str(scaler_1))
+    print(now.strftime("%d/%m/%Y %H:%M:%S") + "\tTotal cal triggers: " + str(scaler_0))
     print(now.strftime("%d/%m/%Y %H:%M:%S") + "\tTotal triggers: " + str(scaler_2))
-    print(now.strftime("%d/%m/%Y %H:%M:%S") + "\tTotal busy: " + str(scaler_3))
+#    print(now.strftime("%d/%m/%Y %H:%M:%S") + "\tTotal busy: " + str(scaler_3))
+    print(now.strftime("%d/%m/%Y %H:%M:%S") + "\tTotal scintillator ANDs: " + str(scaler_3))
 
 if __name__ == "__main__":
     
     now = datetime.now()
     print(now.strftime("%d/%m/%Y %H:%M:%S") + "\tStopping all triggers ...")
 
-    N1081B_device1 = N1081B("pool05940004.cern.ch")
+    N1081B_device1 = N1081B("caenplu-perugia1.cern.ch")
     N1081B_device1.connect()
 
-    N1081B_device2 = N1081B("pool05940001.cern.ch")
+    N1081B_device2 = N1081B("caenplu-perugia2.cern.ch")
     N1081B_device2.connect()
 
     disable_master_trigger()
     disable_pulser()
     
-    send_run_cmd("STOP", 0, "/Data/BLOCKS/USBLF_PCGSC03/", "/home/ams/lontra/log.txt")
+#    send_run_cmd("STOP", 0, "/Data/BLOCKS/USBLF_PCGSC03/", "/home/ams/lontra/log.txt")
 
     now = datetime.now()
     print(now.strftime("%d/%m/%Y %H:%M:%S") + "\tAll Triggers disabled")
--- enable_calibration_trigger.py
+++ enable_calibration_trigger.py
@@ -1,14 +1,14 @@
 import sys
 from datetime import datetime
 from N1081B_sdk import N1081B
-from client_charge_tagger_L0BT_20231022 import *
+#from client_charge_tagger_L0BT_20231022 import *
 
 
 def disable_master_trigger():
     N1081B_device1.configure_digital_generator(N1081B.Section.SEC_C,False,False,False,False)
 
 def enable_calibration():
-    N1081B_device1.configure_digital_generator(N1081B.Section.SEC_C,True,False,False,False)
+    N1081B_device1.configure_digital_generator(N1081B.Section.SEC_C,True,True,True,True)
     N1081B_device1.configure_pulse_generator(N1081B.Section.SEC_B,
                                         N1081B.StatisticMode.STAT_DETERMINISTIC,
                                         250, 1000000, True, True, True, True)
@@ -28,29 +28,32 @@ def reset_scalers():
     print(now.strftime("%d/%m/%Y %H:%M:%S") + "\tScalers reset before calibration trigger")
 
 if __name__ == "__main__":
-    N1081B_device1 = N1081B("pool05940004.cern.ch")
+    N1081B_device1 = N1081B("caenplu-perugia1.cern.ch")
     N1081B_device1.connect()
 
-    N1081B_device2 = N1081B("pool05940001.cern.ch")
+    N1081B_device2 = N1081B("caenplu-perugia2.cern.ch")
     N1081B_device2.connect()
 
     disable_pulser()
-    send_run_cmd("STOP", 0, "/Data/BLOCKS/USBLF_PCGSC03/", "/home/ams/lontra/log.txt")
 
-    N1081B_device2.start_acquisition(N1081B.Section.SEC_B, N1081B.FunctionType.FN_LUT)
+    #    send_run_cmd("STOP", 0, "/Data/BLOCKS/USBLF_PCGSC03/", "/home/ams/lontra/log.txt")
+    
+    N1081B_device1.start_acquisition(N1081B.Section.SEC_D, N1081B.FunctionType.FN_LUT)
     
     now = datetime.now()
     print(now.strftime("%d/%m/%Y %H:%M:%S") + "\tStarting CAL trigger ...")
 
     disable_master_trigger()
+
     now = datetime.now()
     print(now.strftime("%d/%m/%Y %H:%M:%S") + "\tAll triggers disabled")
 
     reset_scalers()
 
-    send_run_cmd("START", 0, "/Data/BLOCKS/USBLF_PCGSC03/", "/home/ams/lontra/log.txt")
+#    send_run_cmd("START", 0, "/Data/BLOCKS/USBLF_PCGSC03/", "/home/ams/lontra/log.txt")
 
     enable_calibration()
+
     now = datetime.now()
     print(now.strftime("%d/%m/%Y %H:%M:%S") + "\tCAL triggers enabled")
 
